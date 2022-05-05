6fee8375e3114ab5f125841353a513bfde370251
Jeff Day <fithbass@gmail.com>
2022-05-05 06:35:46 -0500

    Update bot.js
    Bot now responds to sign in and sign out. made some comments

--- bot.js
+++ bot.js
@@ -6,10 +6,20 @@ const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
-
+//maybe make this a clickable reaction to prevent typos
 client.on('messageCreate', msg => {
+	//input validation - if previous message to @user === 'You are signed in!' reply 'You are alrady signed in.'
   if (msg.content === 'sign in') {
     msg.reply('You are signed in!');
+	//add timestamp and username to DB entry
+  }
+});
+
+client.on('messageCreate', msg => {
+	//input validation - if previous message to @user === 'You are signed out!' reply 'You are alrady signed out.'
+  if (msg.content === 'sign out') {
+    msg.reply('You are signed out!');
+	//add timestamp and username to DB entry
   }
 });
 
